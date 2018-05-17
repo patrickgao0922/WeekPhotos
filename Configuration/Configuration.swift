248cccc58f74574b42a11cb1efa8f3220711ef57
Patrick Gao <patrickgao0922@gmail.com>
2018-05-17 18:51:24 +1000

    initial commit

---
+++ Configuration/Configuration.swift
@@ -0,0 +1,10 @@
+//
+//  Configuration.swift
+//  WeekPhotosTests
+//
+//  Created by Patrick Gao on 17/5/18.
+//  Copyright © 2018 Patrick Gao. All rights reserved.
+//
+
+import Foundation
+
---
+++ WeekPhotosTests/ConfigurationTests.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigurationTests.swift
+//  WeekPhotosTests
+//
+//  Created by Patrick Gao on 17/5/18.
+//  Copyright © 2018 Patrick Gao. All rights reserved.
+//
+
+import Foundation
